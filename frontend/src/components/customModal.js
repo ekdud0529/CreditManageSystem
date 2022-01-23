d3e7ff75500232ae6bdf1de12213b72205b2379b
YE-EUN <musiclove529@naver.com>
2022-01-24 07:14:46 +0900

    static

--- frontend/src/components/customModal.js
+++ frontend/src/components/customModal.js
@@ -4,7 +4,7 @@ import { Button, Modal } from "react-bootstrap";
 class CustomModal extends Component{
     render(){
         return(
-            <Modal dialogClassName={this.props.dialogClassName} show={this.props.show} onHide={this.props.onHide} size="lg" centered>
+            <Modal dialogClassName={this.props.dialogClassName} show={this.props.show} onHide={this.props.onHide} backdrop="static" size="lg" centered>
                 <Modal.Header closeButton>
                     <Modal.Title>
                         {this.props.title}
