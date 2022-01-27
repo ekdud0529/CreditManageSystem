920edba7e7361d769c2e52d553ca134349168532
nature1216 <annekim1216@naver.com>
2022-01-27 11:14:09 +0900

    Feat: button event
    1. 검색 조건 상태 저장
    2. 추가, 삭제 버튼 이벤트 함수 작성
    
    feat #5

--- frontend/src/components/tables.js
+++ frontend/src/components/tables.js
@@ -2,7 +2,96 @@ import React, {Component} from "react";
 import { Button, Form, Row, Col, Stack, Table } from "react-bootstrap";
 
 class Tables extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            // 검색 state
+            year:"",
+            semester:"",
+            target_grade:"",
+            division:"",
+            abeek_bsm : false,
+            abeek_liberal_arts: false,
+            abeek_tech: false,
+            abeek_design: false,
+            title:"",
+
+            GP:"A+", // 검색화면에서 등급 선택
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        }, console.log(e.target.value))
+    }
+
+    handleChangeCheck = (e) => {
+        this.setState({
+            [e.target.name] : e.target.checked
+        }, console.log(e.target.name, e.target.checked))
+    }
+
     render(){
+        var data = this.props.data;
+        var _id = data.length===0?0:data[data.length-1].id+1;
+        var list = [];
+        console.log(_id, data);
+        for(var i=0;i<data.length;i++) {
+            var abeekStr = data[i].abeek_name1 + "/" + data[i].abeek_name2;
+            if(this.props.id === 2 || this.props.id === 3) {
+                list.push(
+                    <tr key={i}>
+                        <td>{data[i].divison_name}</td>
+                        <td>{abeekStr}</td>
+                        <td>{data[i].title}</td>
+                        <td>{data[i].year}</td>
+                        <td>{data[i].semester}</td>
+                        <td>{data[i].credit}</td>
+                        <td>{data[i].GP}</td>
+                        <td><Button id={data[i].id} variant="outline-danger" size="sm" onClick={function(e) {
+                            e.preventDefault();
+                            this.props.onDelete(e.target.id);
+                        }.bind(this)}>삭제</Button></td>
+                    </tr>
+                );
+            } else if(this.props.id === 5) {
+                list.push(
+                    <tr key={i}>
+                        <td>{data[i].course_id}</td>
+                        <td>{data[i].title}</td>
+                        <td>{data[i].division_name}</td>
+                        <td>{abeekStr}</td>
+                        <td>{data[i].year}</td>
+                        <td>{data[i].semester}</td>
+                        <td>{data[i].credit}</td>
+                    <td>
+                        <Form.Select aria-label="Default select example" onChange={this.handleChange} name="GP">
+                            <option value="A+">A+</option>
+                            <option value="A">A</option>
+                            <option value="B+">B+</option>
+                            <option value="B">B</option>
+                            <option value="C+">C+</option>
+                            <option value="C">C</option>
+                            <option value="D+">D+</option>
+                            <option value="D">D</option>
+                            <option value="P">P</option>
+                            <option value="F">F</option>
+                        </Form.Select>
+                    </td>
+                    <td><Button id={i} variant="outline-success" size="sm" onClick={function(e) {
+                        e.preventDefault();
+                        console.log("data[",e.target.id,"].id=",_id);
+                        data[e.target.id].id = _id;
+                        data[e.target.id].GP = this.state.GP;
+                        this.props.onAdd(data[e.target.id]);
+                        _id++;
+                    }.bind(this)}>추가</Button></td>
+                    </tr>
+                )
+            } 
+        }
+
         var _content = null;
         var _button = this.props.id===2?<Button>저장</Button>:<Button>결과</Button>
         switch(this.props.id){
@@ -57,26 +146,7 @@ class Tables extends Component{
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>전공선택</td>
-                                            <td>공학주제/설계</td>
-                                            <td>데이터베이스</td>
-                                            <td>2021</td>
-                                            <td>2</td>
-                                            <td>3</td>
-                                            <td>A+</td>
-                                            <td><Button variant="outline-danger" size="sm">삭제</Button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>전공선택</td>
-                                            <td>공학주제/설계</td>
-                                            <td>병렬분산시스템</td>
-                                            <td>2021</td>
-                                            <td>2</td>
-                                            <td>3</td>
-                                            <td>A+</td>
-                                            <td><Button variant="outline-danger" size="sm">삭제</Button></td>
-                                        </tr>
+                                        {list}
                                     </tbody>
                                 </Table>
                                 <Stack className="my-3 justify-content-center" direction="horizontal" gap={3}>
@@ -90,7 +160,7 @@ class Tables extends Component{
                                 <Row className="my-3">
                                     <Form.Group as={Col} md={3}>
                                         <Form.Label>연도</Form.Label>
-                                        <Form.Select>
+                                        <Form.Select onChange={this.handleChange} name="year">
                                             <option>전체</option>
                                             <option>2021</option>
                                             <option>2020</option>
@@ -101,7 +171,7 @@ class Tables extends Component{
                                     </Form.Group>
                                     <Form.Group as={Col} md={3}>
                                         <Form.Label>학기</Form.Label>
-                                        <Form.Select>
+                                        <Form.Select onChange={this.handleChange} name="semester">
                                             <option>전체</option>
                                             <option>1</option>
                                             <option>2</option>
@@ -109,7 +179,7 @@ class Tables extends Component{
                                     </Form.Group>
                                     <Form.Group as={Col} md={3}>
                                         <Form.Label>학년</Form.Label>
-                                        <Form.Select>
+                                        <Form.Select onChange={this.handleChange} name="target_grade">
                                             <option>전체</option>
                                             <option>1</option>
                                             <option>2</option>
@@ -121,29 +191,29 @@ class Tables extends Component{
                                 <Row className="my-3">
                                     <Form.Group as={Col} md={3}>
                                         <Form.Label>이수구분</Form.Label>
-                                        <Form.Select>
+                                        <Form.Select onChange={this.handleChange} name="division">
                                             <option value="00">전체</option>
-                                            <option value="00">전공필수</option>
-                                            <option value="00">전공선택</option>
-                                            <option value="00">일반선택</option>
-                                            <option value="00">교양</option>
-                                            <option value="00">공통필수</option>
+                                            <option value="02">전공필수</option>
+                                            <option value="03">전공선택</option>
+                                            <option value="05">일반선택</option>
+                                            <option value="01">교양</option>
+                                            <option value="04">공통필수</option>
                                         </Form.Select>
                                     </Form.Group>
                                     <Form.Group as={Col} md={4}>
                                         <Form.Label>공학구분</Form.Label>
                                         <div className="border">
-                                            <Form.Check inline label="BSM" name="abeek" type="checkbox"/>
-                                            <Form.Check inline label="전문교양" name="abeek" type="checkbox"/>
-                                            <Form.Check inline label="공학주제" name="abeek" type="checkbox"/>
-                                            <Form.Check inline label="설계" name="abeek" type="checkbox"/>
+                                            <Form.Check inline label="BSM" name="abeek_bsm" type="checkbox" onChange={this.handleChangeCheck}/>
+                                            <Form.Check inline label="전문교양" name="abeek_liberal_arts" type="checkbox" onChange={this.handleChangeCheck}/>
+                                            <Form.Check inline label="공학주제" name="abeek_tech" type="checkbox" onChange={this.handleChangeCheck}/>
+                                            <Form.Check inline label="설계" name="abeek_design" type="checkbox" onChange={this.handleChangeCheck}/>
                                         </div>          
                                     </Form.Group>
                                     <Col>
                                         <Form.Label column>과목명</Form.Label>
                                         <Row>
                                             <Col md={9}>
-                                                <Form.Control type="text" placeholder="과목명을 입력하세요."></Form.Control>
+                                                <Form.Control type="text" placeholder="과목명을 입력하세요." onChange={this.handleChange} name="title"></Form.Control>
                                             </Col>    
                                             <Col>
                                                 <Button>검색</Button>   
@@ -170,30 +240,7 @@ class Tables extends Component{
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>0000000000</td>
-                                            <td>병렬분산시스템</td>
-                                            <td>전공선택</td>
-                                            <td>공학주제/설계</td>
-                                            <td>2021</td>
-                                            <td>2</td>
-                                            <td>3</td>
-                                            <td>
-												<Form.Select aria-label="Default select example">
-													<option value="1">A+</option>
-													<option value="2">A</option>
-													<option value="3">B+</option>
-													<option value="4">B</option>
-													<option value="5">C+</option>
-													<option value="6">C</option>
-													<option value="7">D+</option>
-													<option value="8">D</option>
-													<option value="9">P</option>
-													<option value="0">F</option>
-												</Form.Select>
-											</td>
-                                            <td><Button variant="outline-success" size="sm">추가</Button></td>
-                                        </tr>
+                                        {list}
                                     </tbody>
                                 </Table>
                             </Form>
--- frontend/src/pages/manage.js
+++ frontend/src/pages/manage.js
@@ -10,7 +10,64 @@ class Manage extends Component{
     constructor(props){
         super(props);
         this.state = {
-            isOpenModal : false
+            isOpenModal : false,
+
+            data : [
+                {
+                    course_id:"0000123123",
+                    divison_name:"전공선택",
+                    abeek_name1:"공학주제",
+                    abeek_name2:"설계",
+                    title:"영상처리",
+                    year:"2021",
+                    semester:"1",
+                    credit:"3",
+                    GP:"A+",
+                    // key: 0,
+                    id: 0
+                },
+                {
+                    course_id:"0000123456",
+                    divison_name:"전공선택",
+                    abeek_name1:"공학주제",
+                    abeek_name2:"설계",
+                    title:"데이터베이스",
+                    year:"2021",
+                    semester:"2",
+                    credit:"3",
+                    GP:"A+",
+                    // key: 1,
+                    id: 1
+                }
+            ],
+            searchData: [
+                {
+                    course_id:"0000123124",
+                    divison_name:"전공선택",
+                    abeek_name1:"공학주제",
+                    abeek_name2:"설계",
+                    title:"병렬분산",
+                    year:"2021",
+                    semester:"2",
+                    credit:"3",
+                    GP:"A+",
+                    // key: 0,
+                    id: 0
+                },
+                {
+                    course_id:"0000123457",
+                    divison_name:"전공선택",
+                    abeek_name1:"공학주제",
+                    abeek_name2:"설계",
+                    title:"마이크로프로세서",
+                    year:"2021",
+                    semester:"1",
+                    credit:"3",
+                    GP:"A+",
+                    // key: 1,
+                    id: 1
+                }
+            ]
         }
     }
 
@@ -22,6 +79,24 @@ class Manage extends Component{
         this.setState({isOpenModal : false});
     }
 
+    onAdd = (newData) => {
+        var _data = this.state.data;
+        _data.push(newData);
+    }
+
+    onDelete = (_id) => {
+        var _data = this.state.data;
+        var list = [];
+        for(var i=0;i<_data.length;i++) {
+            if(_data[i].id !== Number(_id)) {
+                console.log(_data[i].id, Number(_id));
+                list.push(_data[i]);
+            }
+        }
+        console.log(list)
+        this.setState({data: list})
+    }
+
     render(){
         var _title = null;
         var _content = null;
@@ -29,26 +104,26 @@ class Manage extends Component{
             case 1:
                 _title = "이수과목관리";
                 var _modalContent = <div>
-                                        <Tables id={4}></Tables>
-                                        <Tables id={5}></Tables>
+                                        <Tables id={4} data={this.state.searchData}></Tables>
+                                        <Tables id={5} data={this.state.searchData} onAdd={this.onAdd}></Tables>
                                     </div>;
                 _content =  <Container className="manage">
-                                <Tables id={1}></Tables>
-                                <Tables id={2} onOpenModal={this.openModal}></Tables>
+                                <Tables id={1} data={this.state.data}></Tables>
+                                <Tables id={2} onOpenModal={this.openModal} data={this.state.data} onDelete={this.onDelete}></Tables>
                                 <CustomModal dialogClassName="modal-w90" title="과목 검색" content={_modalContent} show={this.state.isOpenModal} onHide={this.closeModal}></CustomModal>
                             </Container>;
                 break;
             case 2:
                 _title = "졸업시뮬레이션";
                 var _modalSearchContent =   <div>
-                                                <Tables id={4}></Tables>
-                                                <Tables id={5}></Tables>
+                                                <Tables id={4} data={this.state.searchData}></Tables>
+                                                <Tables id={5} data={this.state.searchData} onAdd={this.onAdd}></Tables>
                                             </div>;
                 var _modalResultContent =   <div>
                                                 <Tables id={1}></Tables>
                                             </div>;
                 _content =  <Container className="manage">
-                                <Tables id={3} onOpenModal={this.openModal}></Tables>
+                                <Tables id={3} onOpenModal={this.openModal} data={this.state.data} onDelete={this.onDelete}></Tables>
                                 <CustomModal dialogClassName="modal-w90" title="과목 검색" content={_modalSearchContent} show={this.state.isOpenModal} onHide={this.closeModal}></CustomModal>
                             </Container>;
                 break;
