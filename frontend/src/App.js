5e1e117c8a231138acc137c3bd3e71ef2c80d764
ekdud0529 <ekffkddl0529@naver.com>
2022-01-31 20:41:31 +0900

    Feat : connect front and back
    1. 스프링과 리액트 연결
    
    Feat #9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pages from "./pages/pages";
 import {BrowserRouter} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Pages/>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+		  <Pages/>
+		</BrowserRouter>
+	);
 }
 
 export default App;
