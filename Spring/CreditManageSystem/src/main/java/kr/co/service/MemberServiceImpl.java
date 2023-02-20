cdde6bedaa58f8908646e4b8612791b4cb755579
daykim <45174177+ekdud0529@users.noreply.github.com>
2023-02-20 16:13:37 +0900

    Update MemberServiceImpl.java

--- Spring/CreditManageSystem/src/main/java/kr/co/service/MemberServiceImpl.java
+++ Spring/CreditManageSystem/src/main/java/kr/co/service/MemberServiceImpl.java
@@ -13,13 +13,11 @@ public class MemberServiceImpl implements MemberService{
 	@Inject
 	MemberDAO dao;
 	
-	//회원가입
 	@Override
 	public void register(MemberVO vo) throws Exception {
 		dao.register(vo);
 	}
 	
-	// 로그인
 	@Override
 	public MemberVO logIn(MemberVO vo) throws Exception {
 		return dao.logIn(vo);
