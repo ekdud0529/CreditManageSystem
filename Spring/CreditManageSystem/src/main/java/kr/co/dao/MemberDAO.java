121a5a6a25ef1ee3222ef204c6d1b160de946eaa
daykim <45174177+ekdud0529@users.noreply.github.com>
2023-02-20 16:15:28 +0900

    Update MemberDAO.java

--- Spring/CreditManageSystem/src/main/java/kr/co/dao/MemberDAO.java
+++ Spring/CreditManageSystem/src/main/java/kr/co/dao/MemberDAO.java
@@ -4,9 +4,7 @@ import kr.co.vo.MemberVO;
 
 public interface MemberDAO {
 	
-	// 로그인
 	public MemberVO logIn(MemberVO vo) throws Exception;
 	
-	// 회원가입
 	public void register(MemberVO vo) throws Exception;
 }
