0b598acc11c376c5b0639f3852957ebc3c18b09e
daykim <45174177+ekdud0529@users.noreply.github.com>
2023-02-20 16:13:54 +0900

    Update MemberService.java

--- Spring/CreditManageSystem/src/main/java/kr/co/service/MemberService.java
+++ Spring/CreditManageSystem/src/main/java/kr/co/service/MemberService.java
@@ -4,9 +4,7 @@ import kr.co.vo.MemberVO;
 
 public interface MemberService {
 
-	// 회원가입
 	public void register(MemberVO vo) throws Exception;
 	
-	// 로그인
 	public MemberVO logIn(MemberVO vo) throws Exception;
 }
